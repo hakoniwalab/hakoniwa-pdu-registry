a3c576728cf73fe47475d06f110e00d5ae12041f
Takashi Mori <kanetugu2015@gmail.com>
2026-08-02 17:20:41 +0900

    Add Action generator context

---
+++ generators/generate_hako_action_msgs/action_context.py
@@ -0,0 +1,20 @@
+import os
+
+
+class ActionContext:
+    def __init__(self, action_file_path, header_package_name=None):
+        if not os.path.isfile(action_file_path):
+            raise ValueError(
+                f"Error: '{action_file_path}' is not a valid .action file "
+                "(file does not exist or is not a regular file)."
+            )
+        if not action_file_path.endswith(".action"):
+            raise ValueError(f"Error: '{action_file_path}' must have a .action extension.")
+
+        self.action_file_path = action_file_path
+        self.action_name = os.path.splitext(os.path.basename(action_file_path))[0]
+        self.action_package_name = os.path.basename(os.path.dirname(action_file_path))
+        self.msg_package_name = f"{self.action_package_name}_msgs"
+        self.header_package_name = header_package_name or os.getenv(
+            "HAKO_ACTION_HEADER_PKG", "hako_action_msgs"
+        )
